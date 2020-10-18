b424243d873056d5893bc784cce2485e9887c653
Tom Marsh <tom.ian.marsh@gmail.com>
2020-10-18 23:28:20 +1300

    Added some debug target lines and coloured the detection circle to the current stance

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,32 +4,34 @@ extends KinematicBody2D
 export var MOVE_SPEED = 50
 export var DETECTION_RADIUS = 160
 
-onready var timer = $Timer
 onready var sensor = $Sensor
 onready var target_pos = global_position
 
+var target = null
 var stance = "idle"
-var rng = RandomNumberGenerator.new()
 var vec_to_target = Vector2()
 
 
 func _ready():
-	rng.randomize()
+	set_process(true)
 
 
 func _process(delta):
-	timer.start()
+	update()
 
 
 func _physics_process(delta):	
-	if stance == "hunting":
+	if stance == "fighting":
 		# Move toward target
 		vec_to_target = target_pos - global_position
-	else:
+		# Update target position to continue following
+		target_pos = target.position
+	elif stance == "hunting":
 		# Move toward last seen place of target then stop
-		if target_pos != global_position:
+		if target_pos.distance_to(position) > 1:
 			vec_to_target = target_pos - global_position
 		else:
+			stance = "idle"
 			vec_to_target = Vector2()
 	
 	vec_to_target = vec_to_target.normalized()
@@ -37,26 +39,25 @@ func _physics_process(delta):
 
 
 func _draw():
-	draw_circle(Vector2(), DETECTION_RADIUS, Color(0.9, 0.9, 0.9, 0.1))
+	var line_colour = Color(0.0, 0.0, 1.0, 0.7)
+	if stance == "fighting":
+		line_colour = Color(1.0, 0.0, 0.0, 0.7)
+	elif stance == "hunting":
+		line_colour = Color(0.0, 1.0, 0.0, 0.7)
+	draw_line(Vector2(), target_pos - global_position, line_colour, 3)
+	
+	draw_circle(Vector2(), DETECTION_RADIUS, line_colour - Color(0.0, 0.0, 0.0, 0.5))
+
 
 func _on_Sensor_body_entered(body):
 	if body.name == "Player":
+		target = body
 		target_pos = body.position
-		stance = "hunting"
+		stance = "fighting"
 
 
 func _on_Sensor_body_exited(body):
 	if body.name == "Player":
+		target = null
 		target_pos = body.position
-		stance = "idle"
-
-
-func _on_Timer_timeout():
-	rng.randomize()
-	var random_point = Vector2(rng.randi_range(-DETECTION_RADIUS, DETECTION_RADIUS), rng.randi_range(-DETECTION_RADIUS, DETECTION_RADIUS))
-	print(random_point)
-	
-	if stance == "idle":
-		vec_to_target = random_point - global_position
-	
-	timer.start()
+		stance = "hunting"
